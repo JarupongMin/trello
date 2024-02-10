95c8d4ced9f007942395fa273fcb5b64fe9e1c52
JarupongMin <14880@coderacademy.edu.au>
2024-02-10 10:36:48 +1000

    Create cli controller

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import os
-from flask import Flask 
-from init import db, ma, bcrypt, jwt 
+from flask import Flask
+from init import db, ma, bcrypt, jwt
 
-def create_app(): 
+def create_app():
     app = Flask(__name__)
-    
-    # configs 
+
+    # configs
     app.config["SQLALCHEMY_DATABASE_URI"]=os.environ.get("DATABASE_URI")
     app.config["JWT_SECRET_KEY"]=os.environ.get("JWT_SECRET_KEY")
 
-    # connect libraries with flask app 
+    # connect libraries with flask app
     db.init_app(app)
     ma.init_app(app)
     bcrypt.init_app(app)
     jwt.init_app(app)
 
-    return app 
+    from controllers.cli_controller import db_commands
+    app.register_blueprint(db_commands)
 
+    return app
